81e7d34d62bd0cababbd1de71d38af06a93ecc1d
Cara <cara.c.perez@gmail.com>
2024-04-13 15:18:04 -0400

    Adding working files to branch

--- ScheduSquad.DataAccess/Repos/AvailabilityRepository.cs
+++ ScheduSquad.DataAccess/Repos/AvailabilityRepository.cs
@@ -6,32 +6,42 @@ public class AvailabilityRepository : IRepository<Availability>
 {
     private IDbConfiguration _dbConfiguration;
     
-    public AvailabilityRepository(IDbConfiguration dbConfiguration) {
-        _dbConfiguration = dbConfiguration;
-    }
 
-    public string Test() {
-        return "AvailabilityRepository.Test Return String";
-    }
-
-    public void Add(Availability entity)
+    public AvailabilityRepository(IDbConfiguration dbConfiguration)
     {
-        throw new NotImplementedException();
+       _dbConfiguration = dbConfiguration;
+    }
+
+    public void Add(Availability entity, Guid userId)
+    {
+        SqlCommand cmd = new SqlCommand("Add_Availability");
+        cmd.Parameters.Add("@Id", System.Data.SqlDbType.UniqueIdentifier).Value = entity.Id;
+        cmd.Parameters.Add("@UserId", System.Data.SqlDbType.VarChar).Value = userId;
+        cmd.Parameters.Add("@DayEnum", System.Data.SqlDbType.VarChar).Value = entity.DayOfWeek;
+        cmd.Parameters.Add("@StartTime", System.Data.SqlDbType.VarChar).Value = entity.StartTime;
+        cmd.Parameters.Add("@EndTime", System.Data.SqlDbType.VarChar).Value = entity.EndTime;
+        ExecuteLogic(cmd);
     }
 
     public void Delete(Availability entity)
     {
-        throw new NotImplementedException();
+        SqlCommand cmd = new SqlCommand("Delete_Availability");
+        cmd.Parameters.Add("@Id", System.Data.SqlDbType.UniqueIdentifier).Value = entity.Id;
+        ExecuteLogic(cmd);
     }
 
     public IEnumerable<Availability> GetAll()
     {
-        throw new NotImplementedException();
+        SqlCommand cmd = new SqlCommand("Get_All_Availability");
+        cmd.Parameters.Add("@Id", System.Data.SqlDbType.UniqueIdentifier).Value = null;
+        return ExecuteGetAllAvailability(cmd);
     }
 
-    public IEnumerable<Availability> GetAllByParentId(Guid id)
+    public IEnumerable<Availability> GetAllByParentId(Guid id) //ParentId by User
     {
-        throw new NotImplementedException();
+        SqlCommand cmd = new SqlCommand("Get_All_Availability");
+        cmd.Parameters.Add("@Id", System.Data.SqlDbType.UniqueIdentifier).Value = id;
+        return ExecuteGetAllAvailability(cmd);
     }
 
     public Availability GetById(Guid id)
@@ -44,5 +54,56 @@ public class AvailabilityRepository : IRepository<Availability>
         throw new NotImplementedException();
     }
 
-  
+    public void ExecuteLogic(SqlCommand cmd) 
+    {
+        using (SqlConnection con = new SqlConnection(_dbConfiguration.ToString()))
+        {  
+            
+            cmd.CommandType = System.Data.CommandType.StoredProcedure;
+            
+            con.Open();
+
+            cmd.ExecuteNonQuery();
+
+            con.Close();
+        }
+    }
+
+    private IEnumerable<Availability> ExecuteGetAllAvailability(SqlCommand cmd)
+    {
+        IEnumerable<Availability> availabilityList = new List<Availability>();
+        
+        using (SqlConnection con = new SqlConnection(cs))
+        {  
+            
+            cmd.CommandType = System.Data.CommandType.StoredProcedure;
+            
+            con.Open();
+
+            SqlDataReader rdr = cmd.ExecuteReader();  
+            while (rdr.Read())  
+            {
+                var availability = getAvailabilityData(rdr);
+                
+               availabilityList.Append(availability);  
+            }  
+
+            con.Close();
+        }
+
+        return availabilityList;
+    }
+    public Availability getAvailabilityData(SqlDataReader rdr)
+    {
+        DayOfWeek dayOfWeek= (DayOfWeek)Convert.ToInt32(rdr["DayEnum"]);
+
+        var availability = new Availability(
+            new Guid((rdr["Id"]).ToString() ?? string.Empty),     //Id                                         //SquadMaster
+            dayOfWeek,                                           //dayofweek
+            (TimeSpan)rdr["StartTime"],                         //starttime  
+            (TimeSpan)rdr["EndTime"]                           //endtime
+        );
+
+        return availability;
+    }
 }
